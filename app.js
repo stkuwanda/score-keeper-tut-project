488a7c486a6b17c4c00ac72fa3f65fa67ce07e06
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-16 12:42:40 +0200

    implemented logic to end game for both buttons

--- app.js
+++ app.js
@@ -6,12 +6,27 @@ const p2Score = document.querySelector('#p2Score');
 
 let p1ScoreTracker = 0,
 	p2ScoreTracker = 0,
-	winningScore = 5;
+	winningScore = 5,
+	isGameOver = false;
 
 p1Button.addEventListener('click', function () {
-	if (winningScore !== p1ScoreTracker) p1Score.innerText = ++p1ScoreTracker;
+	if (!isGameOver)
+		if (winningScore !== p1ScoreTracker) {
+			p1Score.innerText = ++p1ScoreTracker;
+
+			if (p1ScoreTracker === winningScore) {
+				isGameOver = true;
+			}
+		}
 });
 
 p2Button.addEventListener('click', function () {
-	if (winningScore !== p2ScoreTracker) p2Score.textContent = ++p2ScoreTracker;
+	if (!isGameOver)
+		if (winningScore !== p2ScoreTracker) {
+			p2Score.innerText = ++p2ScoreTracker;
+
+			if (p2ScoreTracker === winningScore) {
+				isGameOver = true;
+			}
+		}
 });
