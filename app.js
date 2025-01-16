0674e3950b59bb3a0b036a034febafa47346c85a
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-16 12:29:18 +0200

    implemented initial winning score checking logic

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const p2Button = document.getElementById('p2Button');
 const p1Score = document.querySelector('#p1Score');
 const p2Score = document.querySelector('#p2Score');
 
-let p1ScoreTracker = 0, p2ScoreTracker = 0;
+let p1ScoreTracker = 0,
+	p2ScoreTracker = 0,
+	winningScore = 5;
 
-p1Button.addEventListener('click', function() {
-  p1Score.innerText = ++p1ScoreTracker;
+p1Button.addEventListener('click', function () {
+	if (winningScore !== p1ScoreTracker) p1Score.innerText = ++p1ScoreTracker;
 });
