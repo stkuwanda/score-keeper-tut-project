dd05cebd4c410a72dcb12ad9e710934d44c6bb45
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-16 12:27:06 +0200

    implemented incrementing logic for score tracking

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('it\'s working!'); // test script
+// alert('it\'s working!'); // test script
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.getElementById('p2Button');
+const p1Score = document.querySelector('#p1Score');
+const p2Score = document.querySelector('#p2Score');
+
+let p1ScoreTracker = 0, p2ScoreTracker = 0;
+
+p1Button.addEventListener('click', function() {
+  p1Score.innerText = ++p1ScoreTracker;
+});
